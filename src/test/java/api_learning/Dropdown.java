204b3d929228881f755e12032c4e327bd134319a
HuyHDL111 <lehuydn111@gmail.com>
2022-11-15 23:58:24 +0700

    lesson 18

---
+++ src/test/java/api_learning/Dropdown.java
@@ -0,0 +1,44 @@
+package api_learning;
+
+import driver.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import support.ui.SelectEx;
+import url.Urls;
+
+
+public class Dropdown {
+    public static void main(String[] args) {
+        WebDriver driver = DriverFactory.getChromeDriver();
+
+        try {
+            // Navigate to the dropdown page
+            driver.get(Urls.BASE_URL.concat(Urls.DROPDOWN_SLUG));
+
+            // Locate the select tab / dropdown
+            WebElement dropdownElem = driver.findElement(By.cssSelector("#dropdown"));
+            SelectEx select = new SelectEx(dropdownElem);
+
+            // Select visible text
+            select.selectOption1();
+            Thread.sleep(2000);
+
+            // Select by index
+            select.selectByIndex(2);
+            Thread.sleep(2000);
+
+            // Select by value
+            select.selectByValue("1");
+            Thread.sleep(2000);
+
+            // Debug purpose only
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+    }
+}
---
+++ src/test/java/api_learning/Dynamic_Control.java
@@ -0,0 +1,57 @@
+package api_learning;
+
+import driver.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import support.ui.WaitForElementEnabled;
+import url.Urls;
+
+import java.time.Duration;
+
+public class Dynamic_Control {
+    public static void main(String[] args) {
+        WebDriver drive = DriverFactory.getChromeDriver();
+
+        try {
+            // Navigate to target page
+            drive.get(Urls.BASE_URL.concat(Urls.DYNAMIC_CONTROL_SLUG));
+
+            // Define 2 form locator
+            By checkboxFormSel = By.id("checkbox-example");
+            By inputFormSel = By.cssSelector("#input-example");
+
+            // Interact with the checkbox form elems
+            WebElement checkboxFormElem = drive.findElement(checkboxFormSel);
+            WebElement checkboxElem = checkboxFormElem.findElement(By.tagName("input"));
+
+            // If the checkbox is not selected then select it
+            if (!checkboxElem.isSelected()) {
+                checkboxElem.click();
+            }
+            // Debug purpose only
+            Thread.sleep(1000);
+
+            // Interact with the input form elems
+            WebElement inputFormElem = drive.findElement(inputFormSel);
+            WebElement inputFieldElem = inputFormElem.findElement(By.tagName("input"));
+            WebElement inputButtonElem = inputFormElem.findElement(By.tagName("button"));
+            // If the text field is not enabled then click on the button
+            if (!inputFieldElem.isEnabled()) {
+                inputButtonElem.click();
+                // Wait until the text field is enable then send keys
+                WebDriverWait wait = new WebDriverWait(drive, Duration.ofSeconds(20L));
+                wait.until(new WaitForElementEnabled(By.cssSelector("#input-example input")));
+            }
+            inputFieldElem.sendKeys("This is the new text");
+
+            // Debug purpose only
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        drive.quit();
+    }
+}
